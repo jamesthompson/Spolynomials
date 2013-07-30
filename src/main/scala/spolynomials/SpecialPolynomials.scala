efa64a993af92fe5740ba819e86b1ce40f022e80
James Thompson <jamesrth@usc.edu>
2013-07-30 15:53:37 -0700

    Working Legendre
    legendres updated and operational nicely

---
+++ src/main/scala/spolynomials/SpecialPolynomials.scala
@@ -0,0 +1,16 @@
+package SPolys
+
+  // Returns a list of legendre Polys (Rational type) up to i
+  def legendres(i: Int)(implicit R: Numeric[Rational]) : List[Poly[Rational]] = {
+    val one = Poly(R.one -> 0)
+    val x = Poly(R.one -> 1)
+    lazy val leg : Stream[Poly[Rational]] = {
+      def loop(pnm1: Poly[Rational], pn: Poly[Rational], n: Int = 1) : Stream[Poly[Rational]] = {
+        pn #:: loop(pn, Poly(R.fromInt(n + 1).reciprocal -> 0) * (
+                    (Poly(R.fromInt(2 * n + 1) -> 1) * pn ) + 
+                    (Poly(R.fromInt(n).unary_- -> 0) * pnm1)), n + 1)
+      }
+      one #:: loop(one, x)
+    }
+    leg.take(i).toList
+  }
