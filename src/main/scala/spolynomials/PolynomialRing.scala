16e85c330edec5cd66dd0e7d29954917b87665bc
James Thompson <jamesrth@usc.edu>
2013-08-01 11:11:59 -0700

    Working quotMod, mod, quot - Euclidean Ring stuff
    Now need to test gcd algo

--- src/main/scala/spolynomials/PolynomialRing.scala
+++ src/main/scala/spolynomials/PolynomialRing.scala
@@ -35,58 +35,31 @@ trait PolynomialRing[F] extends EuclideanRing[Poly[F]] {
 
   def quotMod(x: Poly[F], y: Poly[F]): (Poly[F], Poly[F]) = {
     require(!y.isZero, "Can't divide by polynomial of zero!")
-
-      def zipSum(x: List[F], y: List[F]): List[F] = 
-        x.zip(y).map { case (a,b) => a + b }
     
-      def makeTerms(xs: List[F]): List[(F, Int)] = 
-        xs.zip(((xs.length - 1) to 0 by -1).toList).map({ 
-          case (c, i) => (c, i) })
-    
-    def eval(q: Poly[F], u: Poly[F], n: Int): (Poly[F], Poly[F]) = {
-      val v0 : Term[F] = if(y.isZero) Term(F.zero, 0) else y.maxTerm 
-      (u.isZero || n < 0) match {
-        case true => (q, u) // THIS IS KNACKERED! ARGH!
-        case false => eval(
-          new Poly((u.maxTerm.divideBy(v0.coeff)) :: q.terms), 
-          Poly.fromList(
-            makeTerms(
-              zipSum(
-                u.coeffs, 
-                new Poly(y.terms.map(t => (t * u.maxTerm.divideBy(v0.unary_-.coeff)))).coeffs
-              )
-            ).tail
-          ),
-          n - 1
-        )
-      }
+    def zipSum(x: List[F], y: List[F]): Poly[F] = {
+      val (s, l) = if(x.length > y.length) (y, x) else (x, y)
+      val cs = s.zip(l).map(z => z._1 + z._2) ++ l.drop(s.length)
+      Poly.fromList(cs.zip(((cs.length - 1) to 0 by -1)).tail)
     }
 
-    eval(Poly(F.zero -> 0), x, x.degree - y.degree)
+    def polyFromCoeffsLE(cs: List[F]): Poly[F] =
+      Poly.fromList(cs.zip((0 until cs.length)))
+    
+    def eval(q: List[F], u: Poly[F], n: Int): (Poly[F], Poly[F]) = {
+      lazy val q0 = u.maxTerm.coeff / y.maxTerm.coeff
+      lazy val uprime = zipSum(u.coeffs, y.coeffs.map(_ * q0.unary_-))
+      if(u.isZero || n < 0) (polyFromCoeffsLE(q), u) else eval(q0 :: q, uprime, n - 1)
+    }
+    
+    eval(Nil, x, x.degree - y.degree)
   }
-
 
   def quot(x: Poly[F], y: Poly[F]): Poly[F] = quotMod(x, y)._1
   
-  def mod(x: Poly[F], y: Poly[F]): Poly[F] = {
-    require(!y.isZero, "Can't divide by polynomial of zero!")
-    def eval(u: Poly[F], n: Int): Poly[F] = {
-      lazy val q0 = u.maxTerm.coeff / y.maxTerm.coeff
-      lazy val uprime = zipSum(u.coeffs, y.coeffs.map(_ * q0.unary_-))
-      if(u.isZero || n < 0) u else eval(uprime, n - 1)
-    }
-    eval(x, x.maxOrder - y.maxOrder)
-  }
+  def mod(x: Poly[F], y: Poly[F]): Poly[F] = quotMod(x, y)._2
 
   def gcd(x: Poly[F], y: Poly[F]): Poly[F] = 
   	if(y.isZero && x.isZero) zero else if(y.isZero) x.monic
       else gcd(y, mod(x, y))
-
-  def zipSum(x: List[F], y: List[F]): Poly[F] = {
-    val (s, l) = if(x.length > y.length) (y, x) else (x, y)
-    val cs = s.zip(l).map(z => z._1 + z._2) ++ l.drop(s.length)
-    Poly.fromList(cs.zip(((cs.length - 1) to 0 by -1)).tail)
-  }
-
 
 }
