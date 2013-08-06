2dd50bbd5b70728c97a208b1c92d34d9f1cd1197
James Thompson <jamesrth@usc.edu>
2013-08-06 15:19:38 -0700

    v. close to victory

--- src/main/scala/spolynomials/Polynomial.scala
+++ src/main/scala/spolynomials/Polynomial.scala
@@ -1,10 +1,60 @@
 package spolynomials
 
-import scala.reflect.ClassTag
+import scala.reflect._
 import spire.algebra._
 import spire.math._
 import spire.implicits._
 import spire.syntax._
+
+
+// Univariate polynomial term - No requirements for instantiation whatsoever.
+// n.b. for calculus and division by a scalar a Field[C] instance is required.
+case class Term[C, E](val coeff: C, val exp: E) {
+
+	def eval(x: C)(implicit cring: Ring[C], 
+													conve: ConvertableFrom[E]): C = 
+		cring.times(coeff, cring.pow(x, conve.toInt(exp)))
+
+	def isIndexZero(implicit eord: Order[E],
+													 ering: Ring[E]): Boolean = 
+		eord.eqv(exp, ering.zero)
+
+	def isZero(implicit cord: Order[C],
+											cring: Ring[C]): Boolean = 
+		cord.eqv(coeff, cring.zero)
+
+	def divideBy(x: C)(implicit cfield: Field[C]): Term[C, E] = 
+		Term(cfield.div(coeff, x), exp)
+
+	def der(implicit cring: Ring[C],
+									 ering: Ring[E],
+									 conve: ConvertableFrom[E]): Term[C, E] = 
+		Term(cring.times(coeff, cring.fromInt(conve.toInt(exp))), 
+					ering.minus(exp, ering.one))
+
+	def int(implicit cfield: Field[C],
+									 ering: Ring[E],
+									 conve: ConvertableFrom[E]): Term[C, E] = 
+		Term(cfield.div(coeff, cfield.fromInt(conve.toInt(ering.plus(exp, ering.one)))), 
+					ering.plus(exp, ering.one))
+
+	def termString(implicit cord: Order[C],
+													cring: Ring[C]) = {
+		val pm = cord.compare(coeff, cring.zero)
+		(coeff, exp) match {
+			case (0, i) => ""
+			case (1, 0) => if(pm >= 0) s" + 1" else s" - 1"
+			case (-1, 1) => s" - x"
+			case (1, 1) => s" + x"
+			case (c, 1) => if(pm >= 0) s" + ${c}x" else s" - ${c.unary_-}x"
+			case (1, i) => if(pm >= 0) s" + x^$i" else s" - x^$i"
+			case (c, 0) => if(pm >= 0) s" + ${c}" else s" - ${c.unary_-}"
+			case (c, i) => if(pm >= 0) s" + ${c}x^$i" else s" - ${c.unary_-}x^$i"
+		}
+	}
+
+}
+
 
 // Univariate polynomial class
 class Poly[C: ClassTag, E](val terms: Array[Term[C, E]])
@@ -83,8 +133,14 @@ class Poly[C: ClassTag, E](val terms: Array[Term[C, E]])
 // Companion object for Poly
 object Poly {
 
+	implicit def PR[C: ClassTag, E]: PolynomialRing[C, E] = new PolynomialRing[C, E] {
+		implicit val ctc = classTag[C]
+		implicit val cring = Ring[C]
+		implicit val ering = Ring[E]
+		implicit val cord = Order[C]
+		implicit val eord = Order[E]
+		implicit val conve = ConvertableFrom[E]
+		implicit val cfield = Field[C]
+	}
 
- //  def apply[C: ClassTag, E](terms: Array[Term[C, E]]): Poly[C, E] =
- //  	new Poly(terms)
- 
 }
--- src/main/scala/spolynomials/PolynomialRing.scala
+++ src/main/scala/spolynomials/PolynomialRing.scala
@@ -7,90 +7,77 @@ import spire.implicits._
 import spire.syntax._
 
 // Univariate polynomials form a Euclidean Ring
-// trait PolynomialRing[C, E] extends EuclideanRing[Poly[C, E]] {
+trait PolynomialRing[C, E] extends EuclideanRing[Poly[C, E]] {
 
-//   implicit def ctc: ClassTag[C]
+  implicit def ctc: ClassTag[C]
 
-//   def zero(implicit tR: TermRing[C, E],
-//                     cring: Ring[C],
-//                     ering: Ring[E]) = new Poly(Array(tR.zero))
+  implicit def cring: Ring[C]
+  implicit def ering: Ring[E]
+  implicit def cord: Order[C]
+  implicit def eord: Order[E]
+  implicit def conve: ConvertableFrom[E]
+  implicit def cfield: Field[C]
 
-//   def one(implicit tR: TermRing[C, E],
-//                    cring: Ring[C],
-//                    ering: Ring[E]) = new Poly(Array(tR.one))
+  implicit def tR: Ring[Term[C, E]] = new Ring[Term[C, E]] {
+    def negate(t: Term[C, E]): Term[C, E] = Term(cring.negate(t.coeff), t.exp)
+    def zero: Term[C, E] = Term(cring.zero, ering.zero)
+    def one: Term[C, E] = Term(cring.one, ering.zero)
+    def plus(x: Term[C, E], y: Term[C, E]): Term[C, E] = 
+      Term(cring.plus(x.coeff, y.coeff), y.exp)
+    def times(x: Term[C, E], y: Term[C, E]): Term[C, E] = 
+      Term(cring.times(x.coeff, y.coeff), ering.plus(x.exp, y.exp))
+  }
 
-//   def negate(x: Poly[C, E])(implicit tR: TermRing[C, E],
-//                                      cring: Ring[C]): Poly[C, E] =
-//     new Poly(x.terms.map(tR.negate))
+  def zero = new Poly(Array(tR.zero))
 
-//   def clearZeroesPoly(x: Array[Term[C, E]])
-//                      (implicit tR: TermRing[C, E],
-//                                cring: Ring[C],
-//                                ering: Ring[E]): Poly[C, E] =
-//     new Poly(x.groupBy(_.exp).map({
-//       case (e, l) => l.foldLeft(tR.zero)(tR.plus(_, _))}).toArray)
+  def one = new Poly(Array(tR.one))
 
-//   def plus(x: Poly[C, E], y: Poly[C, E])
-//           (implicit tR: TermRing[C, E],
-//                     cring: Ring[C],
-//                     ering: Ring[E]): Poly[C, E] = 
-//     clearZeroesPoly(x.terms ++ y.terms)
+  def negate(x: Poly[C, E]): Poly[C, E] =
+    new Poly(x.terms.map(tR.negate))
 
-//   def times(x: Poly[C, E], y: Poly[C, E])
-//            (implicit tR: TermRing[C, E],
-//                      cring: Ring[C],
-//                      ering: Ring[E]) : Poly[C, E] = 
-//     clearZeroesPoly(x.terms.flatMap { 
-//       xterm => y.terms.map(yterm => tR.times(yterm, xterm)) })
+  def clearZeroesPoly(x: Array[Term[C, E]]): Poly[C, E] =
+    new Poly(x.groupBy(_.exp).map({
+      case (e, l) => l.foldLeft(tR.zero)(tR.plus(_, _))}).toArray)
 
-//   def quotMod(x: Poly[C, E], y: Poly[C, E])
-//              (implicit tR: TermRing[C, E],
-//                        cfield: Field[C],
-//                        ering: Ring[E],
-//                        conve: ConvertableFrom[E]): (Poly[C, E], Poly[C, E]) = {
-//     require(!y.isZero, "Can't divide by polynomial of zero!")
+  def plus(x: Poly[C, E], y: Poly[C, E]): Poly[C, E] = 
+    clearZeroesPoly(x.terms ++ y.terms)
+
+  def times(x: Poly[C, E], y: Poly[C, E]): Poly[C, E] = 
+    clearZeroesPoly(x.terms.flatMap { 
+      xterm => y.terms.map(yterm => tR.times(yterm, xterm)) })
+
+  def quotMod(x: Poly[C, E], y: Poly[C, E]): (Poly[C, E], Poly[C, E]) = {
+    require(!y.isZero, "Can't divide by polynomial of zero!")
     
-//     def zipSum(x: Array[C], y: Array[C]): Poly[C, E] = {
-//       val (s, l) = if(x.length > y.length) (y, x) else (x, y)
-//       val cs = s.zip(l).map(z => cfield.plus(z._1, z._2)) ++ l.drop(s.length)
-//       new Poly(cs.zip(((cs.length - 1) to 0 by -1)).tail.map({
-//         case (c, e) => Term(c, ering.fromInt(e))
-//         }))
-//     }
+    def zipSum(x: Array[C], y: Array[C]): Poly[C, E] = {
+      val (s, l) = if(x.length > y.length) (y, x) else (x, y)
+      val cs = s.zip(l).map(z => cfield.plus(z._1, z._2)) ++ l.drop(s.length)
+      new Poly(cs.zip(((cs.length - 1) to 0 by -1)).tail.map({
+        case (c, e) => Term(c, ering.fromInt(e))
+        }))
+    }
 
-//     def polyFromCoeffsLE(cs: Array[C]): Poly[C, E] =
-//       new Poly(cs.zip((0 until cs.length)).map({
-//         case (c, e) => Term(c, ering.fromInt(e))
-//         }))
+    def polyFromCoeffsLE(cs: Array[C]): Poly[C, E] =
+      new Poly(cs.zip((0 until cs.length)).map({
+        case (c, e) => Term(c, ering.fromInt(e))
+        }))
     
-//     def eval(q: List[C], u: Poly[C, E], n: Int): (Poly[C, E], Poly[C, E]) = {
-//       lazy val q0 = cfield.div(u.maxOrderTermCoeff, y.maxOrderTermCoeff)
-//       lazy val uprime = zipSum(u.coeffs, y.coeffs.map(yc => cfield.times(yc, cfield.negate(q0))))
-//       if(u.isZero || n < 0) (polyFromCoeffsLE(q.toArray), u) else eval(q0 :: q, uprime, n - 1)
-//     }
+    def eval(q: List[C], u: Poly[C, E], n: Int): (Poly[C, E], Poly[C, E]) = {
+      lazy val q0 = cfield.div(u.maxOrderTermCoeff, y.maxOrderTermCoeff)
+      lazy val uprime = zipSum(u.coeffs, y.coeffs.map(yc => cfield.times(yc, cfield.negate(q0))))
+      if(u.isZero || n < 0) (polyFromCoeffsLE(q.toArray), u) else eval(q0 :: q, uprime, n - 1)
+    }
     
-//     eval(Nil, x, conve.toInt(ering.minus(x.degree, y.degree)))
-//     (zero, one)
-//   }
+    eval(Nil, x, conve.toInt(ering.minus(x.degree, y.degree)))
+    (zero, one)
+  }
 
-//   def quot(x: Poly[C, E], y: Poly[C, E])
-//           (implicit tR: TermRing[C, E],
-//                     cfield: Field[C],
-//                     ering: Ring[E],
-//                     conve: ConvertableFrom[E]): Poly[C, E] =  quotMod(x, y)._1
+  def quot(x: Poly[C, E], y: Poly[C, E]): Poly[C, E] =  quotMod(x, y)._1
   
-//   def mod(x: Poly[C, E], y: Poly[C, E])
-//          (implicit tR: TermRing[C, E],
-//                    cfield: Field[C],
-//                    ering: Ring[E],
-//                    conve: ConvertableFrom[E]): Poly[C, E] =  quotMod(x, y)._2
+  def mod(x: Poly[C, E], y: Poly[C, E]): Poly[C, E] =  quotMod(x, y)._2
 
-//   def gcd(x: Poly[C, E], y: Poly[C, E])
-//          (implicit tR: TermRing[C, E],
-//                    cfield: Field[C],
-//                    ering: Ring[E],
-//                    conve: ConvertableFrom[E]): Poly[C, E] = 
-//   	if(y.isZero && x.isZero) zero else if(y.isZero) x.monic
-//       else gcd(y, mod(x, y))
+  def gcd(x: Poly[C, E], y: Poly[C, E]): Poly[C, E] = 
+  	if(y.isZero && x.isZero) zero else if(y.isZero) x.monic
+      else gcd(y, mod(x, y))
 
-// }
+}
