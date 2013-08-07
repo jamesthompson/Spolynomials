8becf61df462eab42071f1219a70032782d53df0
James Thompson <jamesrth@usc.edu>
2013-08-06 17:46:42 -0700

    Tidied Up

--- src/main/scala/spolynomials/BasicPolynomialTesting.scala
+++ src/main/scala/spolynomials/BasicPolynomialTesting.scala
@@ -8,47 +8,25 @@ import spire.syntax._
 
 object BasicPolynomialTesting extends App {
 
-	println("\n\nCompilation successful...\n\n")
+	import spire.syntax.literals._
 
-	val a: Poly[Double, Int] = new Poly(Array(Term(-0.23, 1), Term(-0.45, 3), Term(0.31, 2)))
-	val b: Poly[Double, Int] = new Poly(Array(Term(0.13, 1), Term(-0.25, 3), Term(0.11, 2)))
+	val a: Poly[Rational, Int] = new Poly(Array(Term(r"1/100", 1), Term(r"2/1", 3), Term(r"-17/19", 2)))
+	val b: Poly[Rational, Int] = new Poly(Array(Term(r"1/2", 1), Term(r"-3/2", 3), Term(r"1/4", 2)))
+
+	val p1 = new Poly(Array(Term(r"2/1", 2), Term(r"1/1", 1)))
+	val p2 = new Poly(Array(Term(r"1/3", 2), Term(r"2/1", 1)))
 
 	println(a.show)
 	println(b.show)
 
-	val c = a + b
+	val d = p1 % p2
+	val e = p1 /~ p2
 
-	println(c.show)
+	println(d.show)
+	println(e.show)
 
-	println(a(1.0))
+	println(a(r"2/1"))
 	println(a.derivative.show)
 	println(a.monic.show)
-
-	// import spire.syntax.literals._
-	// val a = Poly(r"2/1" -> 3, r"1/1" -> 2, r"4/1" -> 0)
-	// val b = Poly(r"1/4" -> 3, r"1/10" -> 2, r"3/1" -> 1)
-
-	// val aeval = a(r"2/1")
-	// val beval = b(r"1/2")
-
-	// println(s"\na = $a")
-	// println(s"b = $b\n")
-	// println(s"a + b = ${a + b}")
-	// println(s"a - b = ${a - b}")
-	// println(s"a * b = ${a * b}")
-	// println(s"gcd(a,b) = ${gcd(a,b)}")
-	// println(s"a % b = ${a % b}")
-	// println(s"a /~ b = ${a /~ b}")
-	// println(s"a /% b = ${a /% b}")
-	// println(s"a.monic = ${a.monic}")
-	// println(s"b.monic = ${b.monic}")
-	// println(s"a's derivative = ${a.derivative}")
-	// println(s"b's derivative = ${b.derivative}")
-	// println(s"a's integral ${a.integral}")
-	// println(s"b's integral ${b.integral}")
-	// println(s"a evaluated with x = 2/1 = $aeval")
-	// println(s"b evaluated with x = 1/2 = $beval")
-
-	// println(s"${SpecialPolynomials.legendres(10)}")
 
 }
--- src/main/scala/spolynomials/Polynomial.scala
+++ src/main/scala/spolynomials/Polynomial.scala
@@ -7,7 +7,7 @@ import spire.implicits._
 import spire.syntax._
 
 
-// Univariate polynomial term - No requirements for instantiation whatsoever.
+// Univariate polynomial term
 // n.b. for calculus and division by a scalar a Field[C] instance is required.
 case class Term[C, E](val coeff: C, val exp: E) {
 
@@ -132,9 +132,7 @@ class Poly[C: ClassTag, E](val terms: Array[Term[C, E]])
 
 object Poly {
 
-  println("Polynomial algebraic engine initialised...")
-
-  implicit def pR[C: ClassTag, E]: PolynomialRing[Double, Int] = new PolynomialRing[Double, Int] {
+  implicit def pRDI[C: ClassTag, E]: PolynomialRing[Double, Int] = new PolynomialRing[Double, Int] {
   	val ctc = classTag[Double]
   	val cring = Ring[Double]
   	val ering = Ring[Int]
@@ -144,79 +142,14 @@ object Poly {
   	val cfield = Field[Double]
   }
 
-  trait PolynomialRing[C, E] extends EuclideanRing[Poly[C, E]] {
-
-  	implicit def ctc: ClassTag[C]
-    implicit def cring: Ring[C]
-    implicit def ering: Ring[E]
-    implicit def cord: Order[C]
-    implicit def eord: Order[E]
-    implicit def conve: ConvertableFrom[E]
-    implicit def cfield: Field[C]
-
-    implicit def tR: Ring[Term[C, E]] = new Ring[Term[C, E]] {
-      def negate(t: Term[C, E]): Term[C, E] = Term(t.coeff.unary_-, t.exp)
-      def zero: Term[C, E] = Term(cring.zero, ering.zero)
-      def one: Term[C, E] = Term(cring.one, ering.zero)
-      def plus(x: Term[C, E], y: Term[C, E]): Term[C, E] = 
-        Term(x.coeff + y.coeff, y.exp)
-      def times(x: Term[C, E], y: Term[C, E]): Term[C, E] = 
-        Term(x.coeff * y.coeff, x.exp + y.exp)
-    }
-
-    def zero = new Poly(Array(tR.zero))
-
-    def one = new Poly(Array(tR.one))
-
-    def negate(x: Poly[C, E]): Poly[C, E] =
-      new Poly(x.terms.map(_.unary_-))
-
-    def clearZeroesPoly(x: Array[Term[C, E]]): Poly[C, E] =
-      new Poly(x.groupBy(_.exp).map({
-        case (e, l) => l.foldLeft(tR.zero)(_ + _)}).toArray)
-
-    def plus(x: Poly[C, E], y: Poly[C, E]): Poly[C, E] = 
-      clearZeroesPoly(x.terms ++ y.terms)
-
-    def times(x: Poly[C, E], y: Poly[C, E]): Poly[C, E] = 
-      clearZeroesPoly(x.terms.flatMap { 
-        xterm => y.terms.map(yterm => yterm * xterm) })
-
-    def quotMod(x: Poly[C, E], y: Poly[C, E]): (Poly[C, E], Poly[C, E]) = {
-      require(!y.isZero, "Can't divide by polynomial of zero!")
-      
-      def zipSum(x: Array[C], y: Array[C]): Poly[C, E] = {
-        val (s, l) = if(x.length > y.length) (y, x) else (x, y)
-        val cs = s.zip(l).map(z => z._1 + z._2) ++ l.drop(s.length)
-        new Poly(cs.zip(((cs.length - 1) to 0 by -1)).tail.map({
-          case (c, e) => Term(c, ering.fromInt(e))
-          }))
-      }
-
-      def polyFromCoeffsLE(cs: Array[C]): Poly[C, E] =
-        new Poly(cs.zip((0 until cs.length)).map({
-          case (c, e) => Term(c, ering.fromInt(e))
-          }))
-      
-      def eval(q: List[C], u: Poly[C, E], n: Int): (Poly[C, E], Poly[C, E]) = {
-        lazy val q0 = u.maxOrderTermCoeff / y.maxOrderTermCoeff
-        lazy val uprime = zipSum(u.coeffs, y.coeffs.map(yc => yc * q0.unary_-))
-        if(u.isZero || n < 0) (polyFromCoeffsLE(q.toArray), u) else eval(q0 :: q, uprime, n - 1)
-      }
-      
-      eval(Nil, x, conve.toInt(x.degree - y.degree))
-      (zero, one)
-    }
-
-    def quot(x: Poly[C, E], y: Poly[C, E]): Poly[C, E] =  quotMod(x, y)._1
-    
-    def mod(x: Poly[C, E], y: Poly[C, E]): Poly[C, E] =  quotMod(x, y)._2
-
-    def gcd(x: Poly[C, E], y: Poly[C, E]): Poly[C, E] = 
-    	if(y.isZero && x.isZero) zero else if(y.isZero) x.monic
-        else gcd(y, mod(x, y))
-
+  implicit def pRRI[C: ClassTag, E]: PolynomialRing[Rational, Int] = new PolynomialRing[Rational, Int] {
+  	val ctc = classTag[Rational]
+  	val cring = Ring[Rational]
+  	val ering = Ring[Int]
+  	val cord = Order[Rational]
+  	val eord = Order[Int]
+  	val conve = ConvertableFrom[Int]
+  	val cfield = Field[Rational]
   }
-
 
 }
